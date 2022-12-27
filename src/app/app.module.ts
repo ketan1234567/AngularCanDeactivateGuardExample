192828efa20929550afa358d37920c0463ff3c85
ketanDeshmukh <deshmukhketan647@gmail.com>
2022-12-28 04:37:21 +0530

    "This is new"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule }   from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { PageNotFoundComponent }  from './page-not-found.component';
+import { CountryModule }  from './country/country.module';
+import { PersonModule }  from './person/person.module';
+import { AppRoutingModule }  from './app-routing.module';
+import { DialogService } from './dialog.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  imports: [     
     BrowserModule,
-    AppRoutingModule
+		CountryModule,
+		PersonModule,
+		AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    PageNotFoundComponent
+  ],
+  providers: [ 
+    DialogService 
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
